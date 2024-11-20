eaecf85c3286f8f65da66995b4cb933d775a5494
Priyanshu <priyanshu@zaamo.co>
2024-11-20 14:58:41 +0530

    adding settings file

--- user_management/user_management/settings.py
+++ user_management/user_management/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)&ok1551#1yo3=7e+4$n60q(m80*$ejy=#b#mf&jls$cihk441'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.getenv('ALLOWED_HOSTS')]
 
 
 # Application definition
